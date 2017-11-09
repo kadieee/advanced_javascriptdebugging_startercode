2f9cde04d651336687939e6e97d49fd755594928
kadie truong <kadietruong16@yahoo.com>
2017-11-09 00:42:24 +0000

    cw

--- js/demo4.js
+++ js/demo4.js
@@ -3,9 +3,10 @@ function magicianMagic() {
     return Math.floor((Math.random() * 100) + 1);
 }
 var secretNumber = magicianMagic();
+debugger
 var input = prompt("Im thinking of a number between 1 - 100: ");
 
-if(secretNumber == input) {
+if(secretNumber ==input) {
     alert("Impressive!");
 } else {
 	alert("Wrong!");
